e7d6ca867f67fa1ac93df213df9d030f4b17516f
Usman Ahmed <anavidreader14@gmail.com>
2024-10-23 11:24:03 +0500

    testing 123

---
+++ test.js
@@ -0,0 +1,11 @@
+console.log("Usman");
+
+let name = "Usman"
+let age = 26;
+
+let university = "Iqra"
+console.log(1 + 19);
+console.log(1 + 19);
+
+console.log(`My name is ${name} and my age is ${age} and I study at ${university} university`)
+
